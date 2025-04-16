1fff47731a6b681ae7bbcfd0e0cee132867485a0
Rohith-Itagi7 <rohititagi2@gamil.com>
2025-04-16 19:38:31 +0530

    if-elif-else

--- 1-10Days-of-python/Day3.py
+++ 1-10Days-of-python/Day3.py
@@ -42,6 +42,7 @@ elif 18.5<= bmi <25:
     print("normal weight")
 elif bmi>=25:
     print("Overweight")
+
 #Multiple If
 height=int(input("Enter your height:"))
 bill=0
@@ -61,7 +62,30 @@ if height >=120:
     if want_photos=="y":
         bill+=3
 
-print(f"Your total height checking cost is {bill}")
+    print(f"Your total height checking cost is {bill}")
 
 else:
     print("Sorry! Your are too short")
+
+#Pizza Order program
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+bill=0
+if size=="S":
+   bill=15
+elif size=="M":
+    bill=20
+elif size=="L":
+    bill=25
+else:
+    print("You have entered wrong input")
+if pepperoni == "Y":
+    if size=="S":
+      bill += 2
+    else:
+      bill+=3
+if extra_cheese=="Y":
+  bill+=1
+print(f"Your final bill is: ${bill}.")
