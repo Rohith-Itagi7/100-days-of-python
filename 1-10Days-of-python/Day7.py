df25ec9de9207202bdd2061f5431237169d60933
Rohith-Itagi7 <rohititagi2@gamil.com>
2025-04-28 13:59:57 +0530

    Day 7:Hangman game

--- 1-10Days-of-python/Day7.py
+++ 1-10Days-of-python/Day7.py
@@ -1,10 +1,34 @@
 # beginner - Hangman
 import random
-list=["baboon","aadvark","camel"]
-word_list=random.choice(list)
+word_list = ["aardvark", "baboon", "camel"]
 
-placeholder=""
-word_len=len(word_list)
-for position in range(word_len):
-    placeholder+="_"
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
 print(placeholder)
+
+game_over=True
+correct_word=[]
+while  game_over:
+
+    guess = input("Guess a letter: ").lower()
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+           display += letter
+           correct_word.append(guess)
+        elif letter in correct_word:
+            display+=letter
+        else:
+           display += "_"
+
+    print(display)
+    if "_" not in display:
+        print("you won")
+        game_over=False
+    else:
+       display+="_"
