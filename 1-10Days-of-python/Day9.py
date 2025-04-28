cec1e1c0f1534879ae1a9a14a189f75e526b6424
Rohith-Itagi7 <rohititagi2@gamil.com>
2025-04-29 00:57:52 +0530

    Day 9:Dictionary_and_Nested_Dictionary

--- 1-10Days-of-python/Day9.py
+++ 1-10Days-of-python/Day9.py
@@ -52,3 +52,29 @@ travel_log = {
    },
 }
 print(travel_log["Germany"]["cities_visted"][2])
+
+#Acution Bulider program
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+bids={}
+continue_bid=True
+while continue_bid:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)
