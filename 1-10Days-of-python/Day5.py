93f2bef5e7b6da9129e90a0dff68d087b5e79c34
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2025-07-25 23:43:05 +0530

    Update Day5.py

--- 1-10Days-of-python/Day5.py
+++ 1-10Days-of-python/Day5.py
@@ -8,7 +8,7 @@ print(fruits)
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 sum=0
 for score in student_scores:
-    sum+=score
+    sum+=score #here it appends score to sum in each iteration
 print(sum)
 
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
