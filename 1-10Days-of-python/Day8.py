205a9ddf3c54dfbe94bf5c5e3db66e59019608a4
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2025-07-29 23:54:15 +0530

    Update Day8.py

--- 1-10Days-of-python/Day8.py
+++ 1-10Days-of-python/Day8.py
@@ -24,7 +24,7 @@ greet_homies(location="Dharwad",name="Rohan")
 def calculate_love_score(name1,name2):
     combined_names = name1 + name2
     lower_names = combined_names.lower()
-    
+    #count method is used to count the repeated number of times item 
     t = lower_names.count("t")
     r = lower_names.count("r")
     u = lower_names.count("u")
